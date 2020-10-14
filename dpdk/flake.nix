e8c63f9163aac7d0d63d9c85bc017962b914915b
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2020-10-14 23:36:31 +0300

    dpdk: DPDK v20.08+ shell with MinGW-w64, GCC, and clang

---
+++ dpdk/flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+
+  outputs = { nixpkgs, ... }: {
+    packages.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
+      let
+        pythonVersion =
+          with python3.sourceVersion; "${major}.${minor}";
+
+        linuxScripts =
+          "${linux.dev}/lib/modules/${linux.dev.version}/source/scripts";
+
+        coff =
+          with python3Packages; callPackage ./coff.nix { };
+
+        commonPackages = [
+          codespell
+          linux.dev
+          meson
+          ninja
+          shellcheck
+          (with python3Packages; [
+            black
+            pip
+            coff
+            pyelftools
+            rope
+            sphinx
+            yapf
+          ])
+        ];
+
+        commonEnv = {
+          DPDK_CHECKPATCH_PATH =
+            "${linuxScripts}/checkpatch.pl";
+          DPDK_CHECKPATCH_CODESPELL =
+            "${codespell.out}/lib/python${pythonVersion}/site-packages/codespell_lib/data/dictionary.txt";
+          DPDK_GETMAINTAINER_PATH =
+            "${linuxScripts}/get_maintainer.pl";
+        };
+
+      in {
+        windows = pkgsCross.mingwW64.mkShell (commonEnv // {
+          nativeBuildInputs = [ commonPackages binutils-unwrapped ];
+
+          CC = "";
+          LD = "";
+          STRIP = "";
+        });
+
+        linux = mkShell (commonEnv // {
+          nativeBuildInputs = [ commonPackages clang ];
+
+          buildInputs = [ numactl pkgconfig ];
+        });
+      };
+  };
+}
